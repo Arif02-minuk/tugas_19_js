0605ec2c5ba7c1e48900979c02b47aa17e67daf5
arif wijanarko <arifpethod@gmail.com>
2023-06-16 09:19:09 +0700

    tugas javascript 19
    untuk niomic

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function perulangan() {
+    var kata = "Semangat...";
+    console.log(kata.repeat(5));
+}
+
+perulangan();
+
+function ulang() {
+    var a = "*";
+    console.log(a.repeat(5));
+    console.log(a.repeat(25));
+    console.log(a.repeat(45));
+    console.log(a.repeat(65));
+    console.log(a.repeat(105));
+    console.log(a.repeat(65));
+    console.log(a.repeat(45));
+    console.log(a.repeat(25));
+    console.log(a.repeat(5));
+}
+ulang();
